fb783d858bc30fc2a680f80ce7f3a8b3ca96dc0f
sjjeong <sjjeong1225@gmail.com>
2019-03-30 21:39:25 +0900

    버튼으로 drawer 열기

--- app/src/main/java/com/googry/googrydrawerlayoutlesson/MainActivity.kt
+++ app/src/main/java/com/googry/googrydrawerlayoutlesson/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.googry.googrydrawerlayoutlesson
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn_open_left.setOnClickListener {
+            dl_root.openDrawer(fl_left_side)
+        }
+        btn_open_left_non_animate.setOnClickListener {
+            dl_root.openDrawer(fl_left_side, false)
+        }
     }
 }
